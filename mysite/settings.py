fc6d81f9fe7b8ae8aac67d09aebc1cbe2b6fc5de
john <johndjango@jon.com>
2024-10-18 17:09:28 +0800

    moved gitignore to base from MyBlog file

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)3jb5fwd4czl+k0f3!hb81n5kh-4qj8grk%hpc_ly)_g_nx)zy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','quentinteo.pythonanywhere.com','.pythonanywhere.com','localhost']#pythonanywhere.com -
 #unused from tutorial
